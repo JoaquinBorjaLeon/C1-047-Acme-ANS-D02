6f01d108676fe3b46f052eeddafb3310d9c3ba00
ArielEC <ariesccap@alum.us.es>
2025-02-26 13:22:25 +0100

    feat: Create Customer entity
    Refs #1

---
+++ src/main/java/acme/entities/Customer.java
@@ -0,0 +1,52 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@ValidString(pattern = "^[A-Z]{2-3}'\'d{6}$")
+	@Mandatory
+	@Column(unique = true)
+	private String				identifier;
+
+	@ValidString(pattern = "^'\'+?'\'d{6,15}$")
+	@Mandatory
+	@Automapped
+	private String				phoneNumber;
+
+	@ValidString
+	@Mandatory
+	@Automapped
+	private String				address;
+
+	@ValidString(max = 50)
+	@Mandatory
+	@Automapped
+	private String				city;
+
+	@ValidString(max = 50)
+	@Mandatory
+	@Automapped
+	private String				country;
+
+	@ValidNumber(max = 500000)
+	@Optional
+	@Automapped
+	private Integer				points;
+}
