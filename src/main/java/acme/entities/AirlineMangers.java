31c1ee56a7c8cbe65e55a89d2311a48cd8f2758c
JoaquinBorjaLeon <joaborleo@alum.us.es>
2025-03-02 11:24:25 +0100

    corrección entidad AirlineManagers (S01-003 #21)

--- src/main/java/acme/entities/AirlineMangers.java
+++ src/main/java/acme/entities/AirlineMangers.java
@@ -5,8 +5,6 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
@@ -21,7 +19,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
