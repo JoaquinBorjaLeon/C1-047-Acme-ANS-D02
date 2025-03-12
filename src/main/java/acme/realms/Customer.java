ab833a573b06e0e05423391fcf39daba499427fe
ArielEC <ariesccap@alum.us.es>
2025-03-12 12:51:11 +0100

    fix: Bound attributes of Customer and create CustomerValidator
    Refs #84

--- src/main/java/acme/realms/Customer.java
+++ src/main/java/acme/realms/Customer.java
@@ -10,43 +10,45 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidCustomer;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidCustomer
 public class Customer extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
-	@ValidString(pattern = "^[A-Z]{2-3}'\'d{6}$")
 	@Mandatory
+	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2-3}'\'d{6}$")
 	@Column(unique = true)
 	private String				identifier;
 
-	@ValidString(pattern = "^'\'+?'\'d{6,15}$")
 	@Mandatory
-	@Column(unique = true)
+	@ValidString(min = 6, max = 16, pattern = "^'\'+?'\'d{6,15}$")
+	@Automapped
 	private String				phoneNumber;
 
-	@ValidString
 	@Mandatory
+	@ValidString(min = 1, max = 255)
 	@Automapped
 	private String				physicalAddress;
 
-	@ValidString(max = 50)
 	@Mandatory
+	@ValidString(min = 1, max = 50)
 	@Automapped
 	private String				city;
 
-	@ValidString(max = 50)
 	@Mandatory
+	@ValidString(min = 1, max = 50)
 	@Automapped
 	private String				country;
 
-	@ValidNumber(max = 500000, min = 0)
 	@Optional
+	@ValidNumber(min = 0, max = 500000)
 	@Automapped
 	private Integer				earnedPoints;
 }
