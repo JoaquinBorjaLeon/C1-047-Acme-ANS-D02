9eb430c8b28e5c309ac25478f0326ab00052e03c
JoaquinBorjaLeon <joaborleo@alum.us.es>
2025-03-12 11:15:20 +0100

    Fix: corrected changes suggested and added validators (S01-005/R1 #77)

---
+++ src/main/java/acme/constraints/LegsValidator.java
@@ -0,0 +1,45 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.entities.Legs;
+
+@Validator
+public class LegsValidator extends AbstractValidator<ValidLegs, Legs> {
+
+	@Override
+	protected void initialise(final ValidLegs annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final Legs leg, final ConstraintValidatorContext context) {
+		if (context == null)
+			throw new IllegalArgumentException("ConstraintValidatorContext cannot be null");
+
+		boolean result;
+
+		if (leg == null)
+			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else {
+			boolean correctCode;
+
+			try {
+
+				String airlineIataCode = leg.getAircraft().getAirline().getIataCode();
+				correctCode = leg.getFlightNumber().substring(0, 3).equalsIgnoreCase(airlineIataCode);
+
+			} catch (Error e) {
+				correctCode = false;
+			}
+
+			super.state(context, correctCode, "*", "acme.validation.legs.flight-number.message");
+		}
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+}
