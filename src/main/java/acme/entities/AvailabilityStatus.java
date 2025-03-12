51d884bdd1b6520b9dfcddb17c1866a67458f2d9
Héctor Guerra <hector.guerraprada@gmail.com>
2025-03-12 21:04:33 +0100

    Fix: Delete files

--- src/main/java/acme/entities/AvailabilityStatus.java
+++
@@ -1,6 +0,0 @@
-
-package acme.entities;
-
-public enum AvailabilityStatus {
-	AVAILABLE, ON_VACATION, ON_LEAVE
-}
--- src/main/java/acme/entities/CrewsDuty.java
+++
@@ -1,7 +0,0 @@
-
-package acme.entities;
-
-public enum CrewsDuty {
-	PILOT, CO_PILOT, LEAD_ATTENDANT, CABIN_ATTENDANT
-
-}
